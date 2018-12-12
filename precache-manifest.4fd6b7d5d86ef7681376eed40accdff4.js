3d1e00aaac8528cf13ab7605a05a6929a06080ca
kdh7565 <kdh7565@sk.com>
2018-12-12 15:57:07 +0900

    Updates

---
+++ precache-manifest.4fd6b7d5d86ef7681376eed40accdff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "68149d0292f97a4f69fc",
+    "url": "/movie_app/static/js/main.68149d02.chunk.js"
+  },
+  {
+    "revision": "769e228087a92a571f8d",
+    "url": "/movie_app/static/js/1.769e2280.chunk.js"
+  },
+  {
+    "revision": "68149d0292f97a4f69fc",
+    "url": "/movie_app/static/css/main.095f9be1.chunk.css"
+  },
+  {
+    "revision": "3733ae6dbda4ec80af4bd66e2bc9cc50",
+    "url": "/movie_app/index.html"
+  }
+];
